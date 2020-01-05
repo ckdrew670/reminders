55fce636b46fdec8f659f6a274fc80084bb2f21e
Charlotte <ckdrew670@hotmail.com>
2020-01-05 11:50:09 +0000

    Add controlled component to App and checked it worked in the console

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,22 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: "",
+        }
+    }
+
+    handleChange = (e) => {
+        this.setState({ text: e.target.value})
+    }
+
+    addReminder = () => {
+        console.log('this.state', this.state);
+    }
+
     render() {
         return (
             <div className="App">
@@ -11,11 +27,13 @@ class App extends Component {
                             type="text"
                             className="form-control"
                             placeholder="I have to ..."
+                            onChange={ this.handleChange }
                         />
                     </div>
                     <button
                         type="button"
                         className="btn btn-success"
+                        onClick={ this.addReminder }
                     >Add</button>
                 </form>
             </div>
