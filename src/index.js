9dc29ea0013d87558b1e0ba55f59e14e47c9afb0
Charlotte <ckdrew670@hotmail.com>
2020-01-06 22:19:21 +0000

    Created separate store file

--- src/index.js
+++ src/index.js
@@ -2,15 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App.js";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
-import reducer from './reducer';
-import './index.css';
+import store from './store';
 
-const store = createStore(
-    reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__
-    && window.__REDUX_DEVTOOLS_EXTENSION__(),
-);
+import './index.css';
 
 ReactDOM.render(
     <Provider store={ store }>
