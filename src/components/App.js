b345fe485b7ba20beef6cfd8ed0388d114b9fee2
Charlotte <ckdrew670@hotmail.com>
2020-01-06 12:34:07 +0000

    Split App into separate components

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Form from './Form';
+import List from './List';
+
+const App = () => (
+
+    <div className="App">
+        <h1 className="title">Reminders</h1>
+        <Form/>
+        <List/>
+    </div>
+       
+);
+
+export default App;
