653ce3220c42b513204a1c17f4ff02c0bd55377a
Charlotte <ckdrew670@hotmail.com>
2020-01-08 16:11:33 +0000

    Updates

---
+++ precache-manifest.c8b7935478ac5fd4a9a06da206a11592.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39cb5440962e0218e7bb14f6df7f2958",
+    "url": "/reminders/index.html"
+  },
+  {
+    "revision": "a58a660f6a1bad4759a2",
+    "url": "/reminders/static/css/main.82e3da15.chunk.css"
+  },
+  {
+    "revision": "ed0b531496c6761cf612",
+    "url": "/reminders/static/js/2.69fe00d8.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/reminders/static/js/2.69fe00d8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a58a660f6a1bad4759a2",
+    "url": "/reminders/static/js/main.0a528a34.chunk.js"
+  },
+  {
+    "revision": "300e2c776259bcf1e2f5",
+    "url": "/reminders/static/js/runtime-main.5e2c8b6d.js"
+  },
+  {
+    "revision": "b86c980126f41cfdcbc2bb8f56c0f8eb",
+    "url": "/reminders/static/media/background.b86c9801.jpg"
+  }
+]);
